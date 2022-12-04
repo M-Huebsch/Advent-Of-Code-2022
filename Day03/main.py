312a57d6d5fd0fd681829bfad3c77bd86826418f
M-Huebsch <marcus.huebsch@gmail.com>
2022-12-04 20:51:28 +0100

    Add Solution for 2nd Part, Clean up Part 1

--- Day03/main.py
+++ Day03/main.py
@@ -8,15 +8,27 @@ buchstaben = []
 werte = 0
 
 with open("Day03/input.txt", "r") as file:
-    for line in file:
-        line = line.strip()
-        length = len(line)//2
-        for i in line[0:length]:
-            if i in line[length:]:
-                buchstaben.append(i)
-                break
-            
-for letter in buchstaben:
-    werte += string.ascii_letters.index(letter) + 1
+    data = [line.strip() for line in file.readlines()]
 
+# Part 1
+werte = 0
+
+for line in data:
+    length = len(line)//2
+    for i in line[0:length]:
+        if i in line[length:]:
+            werte += string.ascii_letters.index(i) + 1
+            break
+           
 print (werte)
+
+# Part 2
+
+werte2 = 0
+
+for i in range (0,len(data),3):
+    s1, s2, s3 = [set(elf) for elf in data[i : i+3]]
+    werte2 += string.ascii_letters.index((s1 & s2 & s3).pop()) + 1
+
+print (werte2)
+
